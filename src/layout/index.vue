1f2b1f3fb2306d87d94f8acfe4c6998d47dff473
smon4k <smon4k@gmail.com>
2022-08-31 09:53:14 +0800

    no message

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -111,8 +111,8 @@ export default {
             });
             // 设置分享文案
             nativeShare.setShareData({
-                icon: 'https://h2o-finance-images.s3.amazonaws.com/h2oMedia/default_avatar.png',
-                link: 'https://futu.win',
+                // icon: 'https://h2o-finance-images.s3.amazonaws.com/h2oMedia/default_avatar.png',
+                link: 'https://apiy.h2ofinance.pro',
                 title: '一站到底',
                 desc: '一站到底',
             })
